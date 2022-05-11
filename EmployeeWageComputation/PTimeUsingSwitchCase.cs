deaa91ca12d60d7f8c58ff329bce3a01ae0cb179
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-11 14:52:15 +0530

    Added to PartTimeEmpWageUsingSwitchCase

---
+++ EmployeeWageComputation/PTimeUsingSwitchCase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class PTimeUsingSwitchCase
+    {
+        public void PartTimeUsingSwitch()
+        {
+            const int isFullTime = 1;
+            const int isPartTime = 2;
+            int empRatePerHour = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case isFullTime:
+                    Console.WriteLine("Employee is Present and is Full Time");
+                    empHrs = 8;
+                    break;
+                case isPartTime:
+                    Console.WriteLine("Employee is Present and is Part Time");
+                    empHrs = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * empRatePerHour;
+            Console.WriteLine("Employee Wage : " + empWage);
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -12,6 +12,7 @@ namespace EmployeeWageComputation
                 Console.WriteLine("\n1. Employee Attendance");
                 Console.WriteLine("2. Calculate Daily Employee Wage");
                 Console.WriteLine("3. Part/Full time employee wage");
+                Console.WriteLine("4. Part/Full time employee wage using switch case");
 
                 Console.WriteLine("0. Exit");
                 Console.Write("Enter your choice : ");
@@ -29,6 +30,10 @@ namespace EmployeeWageComputation
                     case 3:
                         PartTimeEmpWage partTimeEmpWage = new PartTimeEmpWage();
                         partTimeEmpWage.PartTime();
+                        break;
+                    case 4:
+                        PTimeUsingSwitchCase pTimeUsingSwitchCase = new PTimeUsingSwitchCase();
+                        pTimeUsingSwitchCase.PartTimeUsingSwitch();
                         break;
                 }
             } while (choice != 0);
