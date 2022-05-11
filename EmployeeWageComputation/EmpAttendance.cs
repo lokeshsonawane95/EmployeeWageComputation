236e805b1f5eecd55231ce74bd782cc9d8f53da5
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-11 14:29:46 +0530

    Added to EmpAttendance

---
+++ EmployeeWageComputation/EmpAttendance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class EmpAttendance
+    {
+        public void Attendance()
+        {
+            int isFullTime = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == isFullTime)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int choice;
+            do
+            {
+                Console.WriteLine("\n1. Employee Attendance");
+
+                Console.WriteLine("0. Exit");
+                Console.Write("Enter your choice : ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        EmpAttendance empAttendance = new EmpAttendance();
+                        empAttendance.Attendance();
+                        break;
+                }
+            } while (choice != 0);
+        }
+    }
+}
