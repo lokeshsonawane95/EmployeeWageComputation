8b0fbb7b7dacad03b8c57caf49b7423163c35c44
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-11 14:33:29 +0530

    Added to CalcDailyEmpWage

---
+++ EmployeeWageComputation/CalcDailyEmpWage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class CalcDailyEmpWage
+    {
+        public void DailyWage()
+        {
+            int isFullTime = 1;
+            int empRatePerHour = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == isFullTime)
+            {
+                Console.WriteLine("Employee is Present");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
+            }
+            empWage = empHrs * empRatePerHour;
+            Console.WriteLine("Employee Wage : " + empWage);
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -10,6 +10,7 @@ namespace EmployeeWageComputation
             do
             {
                 Console.WriteLine("\n1. Employee Attendance");
+                Console.WriteLine("2. Calculate Daily Employee Wage");
 
                 Console.WriteLine("0. Exit");
                 Console.Write("Enter your choice : ");
@@ -19,6 +20,10 @@ namespace EmployeeWageComputation
                     case 1:
                         EmpAttendance empAttendance = new EmpAttendance();
                         empAttendance.Attendance();
+                        break;
+                    case 2:
+                        CalcDailyEmpWage calcDailyEmpWage = new CalcDailyEmpWage();
+                        calcDailyEmpWage.DailyWage();
                         break;
                 }
             } while (choice != 0);
